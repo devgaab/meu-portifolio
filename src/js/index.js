5b13c7b36d72f138d1d10a844e8743bebcd9432d
Gaab_dev <gabyzinharodrigues230403@gmail.com>
2024-02-04 23:15:58 -0300

    add see more projects

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,5 @@
 // efeito nav scroll
 const navbar = document.querySelector('.navbar');
-
 window.addEventListener('scroll', function () {
     if (this.window.pageYOffset > 0) return navbar.classList.add('active')
     return navbar.classList.remove('active');
@@ -9,7 +8,6 @@ window.addEventListener('scroll', function () {
 // menu mobile
 const hamburguer = document.querySelector(".hamburguer");
 const navMenu = document.querySelector(".navbar__links");
-
 hamburguer.addEventListener("click", () => {
     hamburguer.classList.toggle('active');
     navMenu.classList.toggle('active');
@@ -22,7 +20,18 @@ function typeWrite(elemento) {
     textoArray.forEach((letra, i) => {
         setTimeout(() => elemento.innerHTML += letra, 75 * i);
     });
-}
-
+};
 const titulo = document.querySelector("h1");
 typeWrite(titulo);
+
+// ver mais projetos
+const btnVerMais = document.getElementById('btn-see-more');
+btnVerMais.addEventListener("click", () => {
+    const maisProjetos = document.getElementById('projects-more');
+    maisProjetos.classList.toggle("hide");
+
+    if (maisProjetos.classList.contains('hide')) {
+        return btnVerMais.textContent = 'Ver mais';
+    }
+    btnVerMais.textContent = 'Ver menos';
+});
